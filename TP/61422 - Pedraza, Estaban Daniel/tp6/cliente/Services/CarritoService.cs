9065807192a58048b7f4834e033d4d16d13cd6b2
esteban-82 <esteban.cat129@gmail.com>
2025-06-10 20:20:42 -0300

    Creacion de Carrito.cs en la carpeta Models, definicion de clases.

--- TP/61422 - Pedraza, Estaban Daniel/tp6/cliente/Services/CarritoService.cs
+++ TP/61422 - Pedraza, Estaban Daniel/tp6/cliente/Services/CarritoService.cs
@@ -1,2 +1,34 @@
 using System.Net.Http.Json;
 using cliente.Models;
+
+namespace cliente.Services;
+
+public class CarritoService
+{
+    private readonly HttpClient _http;
+
+    public CarritoService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<Carrito> CrearCarritoAsync()
+    {
+        var response = await _http.PostAsync("api/carritos", null);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadFromJsonAsync<Carrito>();
+    }
+
+    public async Task<Carrito> ObtenerCarritoAsync(string carritoId)
+    {
+        return await _http.GetFromJsonAsync<Carrito>($"api/carritos/{carritoId}");
+    }
+
+    public async Task<Carrito> AgregarProductoAsync(string carritoId, int productoId, int cantidad)
+    {
+        var response = await _http.PutAsync(
+            $"api/carritos/{carritoId}/{productoId}?cantidad={cantidad}", null);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadFromJsonAsync<Carrito>();
+    }
+}
